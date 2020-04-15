306a91fc1523f4071dcd3d119b304f59786804f8
Marcelo Bossle <mablook@gmail.com>
2020-04-15 14:24:34 +0100

    My first playground

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import Context from './components/context/contextStorage/contextStorage';
+import useGlobalState from './components/context/useGlobalState/useGlobalState';
+import Navigation from './containers/Navigation/Navigation';
+import BodyContent from './containers/BodyContent/BodyContent'
+import classes from './App.module.css'
 
 function App() {
+
+    const store = useGlobalState();
+
+    let waitingForSomething = [classes.App];
+
+    if(store.state.appHoldState){
+      waitingForSomething.push(classes.Thinking);
+    }
+
+    useEffect(() => {
+      console.log('[app]: holdstatechange');
+    },[store.appHoldState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={waitingForSomething.join(' ')}>
+      <Context.Provider value={store}>
+        <Navigation />
+        <BodyContent />
+      </Context.Provider>
     </div>
   );
 }
